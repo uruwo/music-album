e97bf4a2ba7fc83b5142f48ceaf43d315bb8c24a
ruo0o0 <ruon0o0@gmail.com>
2022-02-10 23:29:29 +0900

    つながり一覧の表示をフォロイー・フォロワーで切り替え

--- src/components/RelationalUser.vue
+++ src/components/RelationalUser.vue
@@ -1,6 +1,7 @@
 <template>
   <v-card width="600" class="ml-4" min-height="100">
-    <v-card-title class="subtitle-1 py-3">{{ profile.name }}さんがフォロー</v-card-title>
+    <v-card-title class="subtitle-1 py-3" v-if="this.$route.path.includes('followee')">{{ profile.name }}さんがフォロー</v-card-title>
+    <v-card-title class="subtitle-1 py-3" v-else>{{ profile.name }}さんのフォロワー</v-card-title>
     <v-divider></v-divider>
     <div
       v-for="(user, index) in users"
