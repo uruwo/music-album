c925e43acd7351865841610a17cdaa0d5e13c750
ruo0o0 <ruon0o0@gmail.com>
2022-03-01 22:56:11 +0900

    スマホで使えるようにUIを微調整

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -11,7 +11,20 @@
         <v-list-item to="/comments">
           <v-list-item-title>みんなの感想</v-list-item-title>
         </v-list-item>
+        <v-list-item>
+          <v-list-item-title @click="logout">ログアウト</v-list-item-title>
+        </v-list-item>
       </v-list-item-group>
     </v-list>
   </v-navigation-drawer>
 </template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions(['logout'])
+  }
+}
+</script>
