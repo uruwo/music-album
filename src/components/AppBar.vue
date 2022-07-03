dda651b2b2e8eae43f7b880378afb484bd5b9929
ruo0o0 <ruon0o0@gmail.com>
2022-07-03 17:14:13 +0900

    ログイン関連のストアをルートストアから分離

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -8,11 +8,11 @@
       Music Album
     </v-toolbar-title>
 
-    <v-toolbar-items v-if="$store.state.login_user">
+    <v-toolbar-items v-if="$store.state.login.login_user">
       <v-btn class="visible" text @click="logout">ログアウト</v-btn>
     </v-toolbar-items>
 
-    <v-tabs v-if="$store.state.login_user" right optional>
+    <v-tabs v-if="$store.state.login.login_user" right optional>
       <v-tab to="/home">ホーム</v-tab>
       <v-tab to="/explore">探索</v-tab>
       <v-tab to="/albums">アルバム</v-tab>
@@ -22,11 +22,11 @@
 
     <v-spacer></v-spacer>
 
-    <v-toolbar-items v-if="!$store.state.login_user && this.$route.path === '/'">
+    <v-toolbar-items v-if="!$store.state.login.login_user && this.$route.path === '/'">
       <v-btn text to="/login">ログイン</v-btn>
     </v-toolbar-items>
 
-    <v-app-bar-nav-icon @click="toggleSideMenu" v-if="$store.state.login_user"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="toggleSideMenu" v-if="$store.state.login.login_user"></v-app-bar-nav-icon>
   </v-app-bar>
 </template>
 
---
+++ src/store/modules/login.js
@@ -0,0 +1,53 @@
+import firebase from 'firebase'
+import axios from 'axios'
+
+export default {
+    state: {
+        login_user: null,
+        firebase_auth_token: ''
+    },
+    mutations: {
+        async setLoginUser (state, {user, token}) {
+            state.login_user = user
+            state.firebase_auth_token = token
+        },
+        deleteLoginUser (state) {
+            state.login_user = null
+        }
+    },
+    actions: {
+        login () {
+            const google_auth_provider = new firebase.auth.GoogleAuthProvider()
+            firebase.auth().signInWithRedirect(google_auth_provider)
+        },
+      
+        async logout ({state, rootGetters}) {
+            if (state.login_user.isAnonymous) {
+                const uid = rootGetters.uid
+        
+                axios.delete(state.api_follow + '/user', {data: {user_id: uid}, headers: rootGetters.headers})
+        
+                await firebase.firestore().collection(`users/${uid}/profile`).get().then(snapshot => snapshot.forEach(doc => {
+                    firebase.firestore().collection(`users/${uid}/profile`).doc(doc.id).delete()
+                }))
+        
+                await firebase.firestore().collection(`users/${uid}/album`).get().then(snapshot => snapshot.forEach(doc => {
+                    firebase.firestore().collection(`users/${uid}/album`).doc(doc.id).delete()
+                }))
+        
+                await firebase.firestore().collection(`users/${uid}/albums`).get().then(snapshot => snapshot.forEach(doc => {
+                    firebase.firestore().collection(`users/${uid}/albums`).doc(doc.id).delete()
+                }))
+            }
+    
+            firebase.auth().signOut()
+        },
+    
+        setLoginUser ({ commit }, {user, token}) {
+            commit('setLoginUser', {user: user, token: token})
+        },
+        deleteLoginUser ({commit}) {
+            commit('deleteLoginUser')
+        }
+    }
+}
